ef048531892e2db7beb11b56709eadb4a3e0bf04
Shehzad Iqbal <shehzaddiqbal@gmail.com>
2024-11-08 22:31:31 +0500

    new added commit

---
+++ app.js
@@ -0,0 +1,52 @@
+// Basics of writing and Showing
+// console.log("Hello to Saylani")
+// document.write("<h3>This message is Powered By Javascript</h3>")
+// alert("This is our Main Page")
+// confirm("Do you want to proceed more?")
+// prompt("Enter Your Name: ")
+
+// Variable / Constant
+
+// const jar1 = 123; // To store the Integer data
+// console.log("To store the Integer data",jar)
+
+// var jar = 12.3; // To store the float data
+// console.log("To store the float data", jar)
+
+// var jar = "Hammad"; // To store the String data
+// console.log("To store the String data", jar)
+
+// var jar = false; // To store the Boolean data
+// console.log("To store the Boolean data",jar)
+
+// const hunain= null;
+// document.write(hunain)
+
+// Arthematic Operation
+
+var x = 100 + 50;
+console.log(x)
+
+var x = 100 - 50;
+console.log(x)
+
+var x = 100 * 50;
+console.log(x)
+
+var x = 100 / 50;
+console.log(x)
+
+// Write a Table that user input any number, the whole table would generate...!
+
+
+var input = prompt("Enter the number")
+document.write(input + " X 1 =" + input*1 + "<br>" )
+document.write(input + " X 2 =" + input*2 + "<br>" )
+document.write(input + " X 3 =" + input*3 + "<br>" )
+document.write(input + " X 4 =" + input*4 + "<br>" )
+document.write(input + " X 5 =" + input*5 + "<br>" )
+document.write(input + " X 6 =" + input*6 + "<br>" )
+document.write(input + " X 7 =" + input*7 + "<br>" )
+document.write(input + " X 8 =" + input*8 + "<br>" )
+document.write(input + " X 9 =" + input*9 + "<br>" )
+document.write(input + " X 10 =" + input*10 + "<br>" )
