b499b51af4c1cf1652e466e8fefeb44da5c0dae7
Shehzad Iqbal <shehzaddiqbal@gmail.com>
2024-11-10 22:01:00 +0500

    files added

--- app.js
+++ app.js
@@ -24,29 +24,53 @@
 
 // Arthematic Operation
 
-var x = 100 + 50;
-console.log(x)
+// var x = 100 + 50;
+// console.log(x)
 
-var x = 100 - 50;
-console.log(x)
+// var x = 100 - 50;
+// console.log(x)
 
-var x = 100 * 50;
-console.log(x)
+// var x = 100 * 50;
+// console.log(x)
 
-var x = 100 / 50;
-console.log(x)
+// var x = 100 / 50;
+// console.log(x)
 
 // Write a Table that user input any number, the whole table would generate...!
 
+// var input = prompt("Enter the number")
+// document.write(input + " X 1 =" + input*1 + "<br>" )
+// document.write(input + " X 2 =" + input*2 + "<br>" )
+// document.write(input + " X 3 =" + input*3 + "<br>" )
+// document.write(input + " X 4 =" + input*4 + "<br>" )
+// document.write(input + " X 5 =" + input*5 + "<br>" )
+// document.write(input + " X 6 =" + input*6 + "<br>" )
+// document.write(input + " X 7 =" + input*7 + "<br>" )
+// document.write(input + " X 8 =" + input*8 + "<br>" )
+// document.write(input + " X 9 =" + input*9 + "<br>" )
+// document.write(input + " X 10 =" + input*10 + "<br>" )
 
-var input = prompt("Enter the number")
-document.write(input + " X 1 =" + input*1 + "<br>" )
-document.write(input + " X 2 =" + input*2 + "<br>" )
-document.write(input + " X 3 =" + input*3 + "<br>" )
-document.write(input + " X 4 =" + input*4 + "<br>" )
-document.write(input + " X 5 =" + input*5 + "<br>" )
-document.write(input + " X 6 =" + input*6 + "<br>" )
-document.write(input + " X 7 =" + input*7 + "<br>" )
-document.write(input + " X 8 =" + input*8 + "<br>" )
-document.write(input + " X 9 =" + input*9 + "<br>" )
-document.write(input + " X 10 =" + input*10 + "<br>" )
+// Concatination
+// var input = prompt("Enter the number")
+// document.write(input + " X 1 =" + input*1 + "<br>" )
+
+// var a = prompt("Input the Any name")
+// if (a == "Hammad Sheikh") {
+//     alert("This is AI Class")
+// }else{
+//     alert("You are not eligible for this Class")
+// }
+
+// Array
+
+var names = "Saylani"
+var name1 = "walfare"
+var name2 = "Karachi"
+console.log(name2)
+            // 0      ,   1       , 2
+// var city = ["Karachi", "Lahore", "Islamabad"]
+// console.log(city[2])
+// console.table(city)
+
+var dataTypes = ["Sialkot", 12, true, 12.121, "Pakistan"]
+console.table(dataTypes)
